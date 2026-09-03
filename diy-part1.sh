1dd16d44b3b76e560f3f35c182b79644c61a15bd
srfj <monkeycode-ai@chaitin.com>
2026-09-03 13:12:58 +0000

    feat: replace argon theme with jerrykuku latest, remove upnp plugin Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 #git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/openwrt-passwall
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/luci-app-passwall
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
